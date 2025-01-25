2c2ad407839e47408ac27f9f47819f30254dbc82
ashwinishankwaar <ashwinishankwaar@gmail.com>
2025-01-25 13:59:00 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Target100In30DaysEnd16JanLeetCode/LinkedList/RemovedLinkedListElement.java
+++ src/Target100In30DaysEnd16JanLeetCode/LinkedList/RemovedLinkedListElement.java
@@ -3,20 +3,32 @@ package Target100In30DaysEnd16JanLeetCode.LinkedList;
 public class RemovedLinkedListElement {
     public ListNode removeElements(ListNode head, int val) {
         if(head == null) return null;
-
-//        if(head.val == val) head = head.next;
+//        ListNode dummy = new ListNode();
+//        ListNode dummyTemp = dummy;
 //        ListNode temp = head;
 //        while(temp!=null){
-//            if(temp.next.val == val){
-//                temp.next = temp.next.next;
-//            }else{
-//                temp = temp.next;
+//            ListNode node = new ListNode(temp.val);
+//            if(temp.val != val) {
+//                dummyTemp.next = node;
+//                dummyTemp = dummyTemp.next;
 //            }
+//            temp = temp.next;
 //        }
-        return head;
+//        return dummy.next;
+
+        //optimised way using recursion
+        if(head == null) return null;
+        head.next = removeElements(head.next,val);
+        if(head.val == val){
+            return head.next;
+        }else{
+            return head;
+        }
+
     }
     public static void main(String[] args) {
-        int[] arr = new int[]{7,7,7,7,7};
+        int[] arr = new int[]{1,2,3,6,4,6,5};
+        int val = 6;
         ListNode head = new ListNode(arr[0]);
         ListNode temp = head;
         for(int i=1; i< arr.length; i++) {
@@ -24,6 +36,7 @@ public class RemovedLinkedListElement {
             temp.next = node;
             temp = temp.next;
         }
+//        System.out.println(head.val);
 //        ListNode head = new ListNode(6,
 //                new ListNode(2,
 //                        new ListNode(6,
@@ -32,7 +45,7 @@ public class RemovedLinkedListElement {
 //                                                new ListNode(6,
 //                                                        new ListNode(5,
 //                                                                new ListNode(6))))))));
-        head = new RemovedLinkedListElement().removeElements(head,7);
+        head = new RemovedLinkedListElement().removeElements(head,val);
         while (head!=null){
             System.out.println(head.val);
             head = head.next;
